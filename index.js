641a4a7d8f138b04b86f36be5541931d89f48318
LeaoMartelo <leaomartelo@gmail.com>
2025-12-30 19:51:59 -0300

    push

--- index.js
+++ index.js
@@ -1,3 +1,57 @@
-register('command', () => {
-    ChatLib.chat("template_module command ran");
-}).setName('template_module');
+var selected_slot = FileLib.read("./config/ChatTriggers/modules/IceSprayBind-1.21/slot.cfg");
+
+function get_slot(){
+    selected_slot = FileLib.read("./config/ChatTriggers/modules/IceSprayBind-1.21/slot.cfg");
+}
+
+function chat_msg(string) {
+    ChatLib.chat("&bIceSprayBind&f> " + string);
+}
+
+function show_help() {
+    chat_msg("Use /set_slot <1-9> to use as your extra bound slot.");
+    chat_msg("Set its keybind at the minecraft settings");
+    chat_msg("Check the current slot with /what_slot");
+}
+
+const key_bind = Client.getKeyBindFromKey(Keyboard.KEY_F, "IceSprayBind", "");
+
+register("command", () => {
+    show_help();
+}).setName("IceSprayHelp");
+
+
+
+register("command", function(args) {
+    if(parseInt(args) > 9) {
+        chat_msg("Slot must be between 1-9");
+        return;
+    }
+
+    if(parseInt(args) < 0) {
+        chat_msg("Slot must be between 1-9");
+        return;
+    }
+
+    FileLib.write("./config/ChatTriggers/modules/IceSprayBind-1.21/slot.cfg", parseInt(args));
+    chat_msg("Set slot to: " + parseInt(args));
+    get_slot();
+}).setName("set_slot");
+
+
+register("command", () => {
+    chat_msg("Selected slot -> " + selected_slot);
+}).setName("what_slot");
+
+register("tick", () => {
+
+    if(key_bind.isPressed()) {
+
+//        chat_msg(selected_slot);
+
+        Player.setHeldItemIndex(selected_slot - 1);
+
+    }
+
+});
+
